d7b28f5adb3f073cc9c39b8f9f7e7f61634e9818
Alexandra Simon <s.simonalexandra@gmail.com>
2021-03-13 14:15:43 +0100

    edit cout

--- 1hw/Newton.cpp
+++ 1hw/Newton.cpp
@@ -1,28 +1,9 @@
-/*
-Írjuk meg a Newton iterátort most általánosan úgy, hogy a függvény és a
-deriváltja is az előbb bemutatott módon legyen átadva és a kezdő
-paraméter típusa is általános T legyen.
-*/
-
-/* 
-Newton([](double x ) { return x x 612.0; }, [](double x ) { return 2.0*x ; }, 10.0);
-*/
-
-// 1) fgv x_i helyen érték kiszamitasara egy fgv
-
-// 2) f(x) derivaltjara egy fgv
-
-// 3) maga a N-R formula for ciklusban, amig kisebb,egyenlo mind a megadott epsilon 
-
 #include <iostream>
 #include <cmath>
 #include <iostream>
+using namespace std;  
 
-using namespace std;  //megnézni mashogy
-
-template<typename T, typename F, typename G>  // G nélkül??
-
-
+template<typename T, typename F, typename G>  
 
 T Newton(F fgv, G d_fgv, T x_i, double err) // err -> ACCURACY
 {
@@ -41,6 +22,5 @@ T Newton(F fgv, G d_fgv, T x_i, double err) // err -> ACCURACY
 
 int main()
 {
-    cout<< Newton([](double x ) { return x*x-612.0;}, [](double x ) { return 2.0*x ;}, 10.0, 0.0001);
-    cout<<"lol";
+    cout << "Root of given function: " << Newton([](double x ) { return x*x-612.0;}, [](double x ) { return 2.0*x ;}, 10.0, 0.0001);
 }
