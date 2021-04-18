f8ce44ad446d7f916ede7cfcf7088bcff78c15ab
Alexandra Simon <s.simonalexandra@gmail.com>
2021-04-18 18:29:26 +0200

    hiperbola

---
+++ 3hw/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// Derivált - Richardson-extrapoláció
+
+double Fun(double x)    //  hiperbola
+{
+    const int  F = 900, a = 200, h = 35;
+    const double  q = 1.8;
+
+    return  F/q * ( cosh((q/F)*x) - cosh((q*a)/(2*F))  ) + h;
+
+}
+
+template <typename T>
+T dFun(double x)
+{
+
+
+
+
+}
+
+
+// Középpontos szabály:
+
+
+
+
+int main()
+{
+    
+    return 0;
+}
