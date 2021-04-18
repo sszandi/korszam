29c2dbdbc54d5dc5dc20dc0813c99ec08c614922
Simon Alexandra <80391186+sszandi@users.noreply.github.com>
2021-04-18 20:52:55 +0200

    added mid, trapezoidal

--- 3hw/main.cpp
+++ 3hw/main.cpp
@@ -2,29 +2,80 @@
 #include <cmath>
 using namespace std;
 
-// Derivált - Richardson-extrapoláció
+
+// Derivált - Richardson-extrapoláció:
 
 double Fun(double x)    //  hiperbola
 {
-    const int  F = 900, a = 200, h = 35;
+    const int  F = 900, a = 200, H = 35;
     const double  q = 1.8;
 
-    return  F/q * ( cosh((q/F)*x) - cosh((q*a)/(2*F))  ) + h;
-
+    return  F/q * ( cosh((q/F)*x) - cosh((q*a)/(2*F))  ) + H;
 }
 
 template <typename T>
-T dFun(double x)
+T dFun2(T x, double h)  // másodrendű közelítés
 {
+    return Fun(x + h)-Fun(x - h) )/2*h;
+}
 
-//no
+template <typename T> 
+T dFun4(T x, double h) // negyedrendű közelítés
+{
+    return ( 4*dFun2(x,h)-dFun2(x,h) )/2*h;
+}
 
+template <typename T>  
+T dFun(double x, double h)  // hatodrendű közelítés
+{
+    return ( 16*dFun4(x, h)-dFun4(x, 2*h) )/15; //h kint
+}
+
+
+// Integrál - Középpontos szabály:
+
+double mid(double x,double a, double b, int n)  //feloszt kint??
+{
+    double dx, M, m;
+    dx = fabs(b-a)/n;
+    
+    for (int i=0; i<n; i++)
+    {
+        M += Fun( (a+(i*dx))/2) * dx;
+    }
+
+    return M;
 
 }
 
 
-// Középpontos szabály:
+// trapéz-szabály:
 
+double trpz(double x, double a, double b, double n)
+{
+    double dx, sum, result;
+    dx = fabs(b-a)/n;
+    for( int i=1; i<n; i++ )
+    {
+        sum +=Fun(a+i*dx);
+    }
+    
+    result = ( dx/2 )*( Fun(a)+Fun(b)+2*sum );
+
+    return result;
+}
+
+
+
+
+// 1/3 Simpson-szabály:
+
+double simpson(double x, double a, double b, int n)
+{
+
+
+
+}
 
 
 
